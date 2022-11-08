929903e179737675398a73e26654eed12b5c720e
Amal4m41 <amalsunil117@gmail.com>
2022-11-08 21:15:13 +0400

    Added script to rename files in system

---
+++ changeFileExtension/demo/s.txt.md.md.md.md.md.md
@@ -0,0 +1,4 @@
+this is the file content.
+has many lines
+like this line.
+Bye.
---
+++ changeFileExtension/renameFiles.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const fs = require("fs");
+let filesList = [];
+
+function getListOfFilesFromDir(dir) {
+    fs.readdirSync(dir).forEach(fileOrDir => {
+        const pathValue = path.join(dir, fileOrDir);
+        fs.statSync(pathValue).isDirectory() ?
+            getListOfFilesFromDir(pathValue) :
+            filesList.push(pathValue);
+    });
+}
+
+function renameFilesWithExtension(extension, typeOfRename) {
+    filesList.forEach(file => fs.rename(
+        file,
+        typeOfRename(file, extension),
+        (err) => {
+            if (err) console.log(err);
+        }
+    ));
+}
+
+const addExtension = (fileName, extension) => fileName + extension;
+
+// const dirPath = 'C:\\Users\\Amal Sunil\\Desktop\\dump';
+const dirPath = __dirname + '/demo';
+
+getListOfFilesFromDir(dirPath);
+console.log('List of files: ', filesList);
+// renameFilesWithExtension('.md', addExtension);
